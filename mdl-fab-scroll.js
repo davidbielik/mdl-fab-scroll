09bc20ff4f70f053600e5b59450107e62d6aaefc
David Bielik <dbielik90@gmail.com>
2016-11-07 17:30:01 -0500

    Fixed bug to re-run on route changes

--- mdl-fab-scroll.js
+++ mdl-fab-scroll.js
@@ -1,19 +1,31 @@
 (function() {
     angular
         .module('mdl-fab-scroll', [])
-        .run(function() {
-            setTimeout(function() {
-                var lastPosition;
-                var hideClass = 'mdl-fab-scroll-hide';
-                var container = document.querySelector('.mdl-layout__content');
-                container = angular.element(container);
-                container.on('scroll', function(event) {
-                    var position = event.target.scrollTop || 0;
-                    angular.element(document.body)[position > lastPosition ? 'addClass' : 'removeClass'](hideClass);
-                    lastPosition = position;
+        .run(function($timeout, $rootScope) {
+            init();
+            $timeout(function(){
+                // var $rootScope = angular.element(document.body).injector().get('$rootScope');
+                $rootScope.$on('$routeChangeSuccess', init);
+            });
+
+
+            function init(){
+                $timeout(function(){
+                    var lastPosition;
+                    var hideClass = 'mdl-fab-scroll-hide';
+                    var container = document.querySelector('.mdl-layout__content');
+                    container = angular.element(container);
+                    container.on('scroll', function(event) {
+                        var position = event.target.scrollTop || 0;
+                        angular.element(document.body)[position > lastPosition ? 'addClass' : 'removeClass'](hideClass);
+                        lastPosition = position;
+                    });
                 });
-            }, 0);
+            }
         });
+
+
+
 
     var css = document.createElement('style');
     css.innerHTML = '.mdl-button--fab {transition-property: transform;}body.mdl-fab-scroll-hide .mdl-button--fab {animation: transform 3s;transform: scale(0,0) rotate(360deg)}';
